f1586c75d3b287c169f7fbe25c2b49558ab639d8
Josué Ramos <108350617+Niall-swift@users.noreply.github.com>
2022-10-06 12:18:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-document.getElementById('input').addEventListener('change', () => {
-
-
-  if (document.body.className.indexOf('dark') === -1) {
-
-      document.body.classList.add('dark')
-  } else {
-      document.body.classList.remove('dark')
-
-  }
-})
-
 
 
 const header = Document.querySelector("header");
